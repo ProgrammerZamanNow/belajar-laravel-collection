49bdac7c224bd2d0ee4112f5b6422e5ed8ba13a5
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-05 01:27:01 +0700

    belajar laravel collection

---
+++ tests/Feature/CollectionTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CollectionTest extends TestCase
+{
+    public function testCreateCollection()
+    {
+        $collection = collect([1, 2, 3]);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+    }
+
+}
