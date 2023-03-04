80575b1faa2058c17fc4040e3b6248fccc8f99a8
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-05 01:37:35 +0700

    Manipulasi Collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -17,9 +17,20 @@ class CollectionTest extends TestCase
     public function testForEach()
     {
         $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        foreach ($collection as $key => $value){
+        foreach ($collection as $key => $value) {
             $this->assertEquals($key + 1, $value);
         }
+    }
+
+    public function testCrud()
+    {
+        $collection = collect([]);
+        $collection->push(1, 2, 3);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+
+        $result = $collection->pop();
+        $this->assertEquals(3, $result);
+        $this->assertEqualsCanonicalizing([1, 2], $collection->all());
     }
 
 
