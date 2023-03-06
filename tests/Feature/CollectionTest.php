185d473197618074b9cc337d7ac1f2cf95b21c26
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-06 17:28:01 +0700

    Zipping

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -99,5 +99,39 @@ class CollectionTest extends TestCase
 
     }
 
+    public function testZip()
+    {
+        $collection1 = collect([1,2,3]);
+        $collection2 = collect([4,5,6]);
+        $collection3 = $collection1->zip($collection2);
+
+        $this->assertEquals([
+            collect([1,4]),
+            collect([2,5]),
+            collect([3,6]),
+        ], $collection3->all());
+    }
+
+    public function testConcat()
+    {
+        $collection1 = collect([1,2,3]);
+        $collection2 = collect([4,5,6]);
+        $collection3 = $collection1->concat($collection2);
+
+        $this->assertEqualsCanonicalizing([1,2,3,4,5,6], $collection3->all());
+    }
+
+    public function testCombine()
+    {
+        $collection1 = collect(["name", "country"]);
+        $collection2 = collect(["Eko", "Indonesia"]);
+        $collection3 = $collection1->combine($collection2);
+
+        $this->assertEqualsCanonicalizing([
+            "name" => "Eko",
+            "country" => "Indonesia"
+        ], $collection3->all());
+    }
+
 
 }
