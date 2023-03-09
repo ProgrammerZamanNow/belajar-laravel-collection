31f8ec19423321c6d77b08cfa7fd7d2e9340f3db
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-10 01:39:29 +0700

    Chunked

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -349,5 +349,17 @@ class CollectionTest extends TestCase
 
     }
 
+    public function testChunk()
+    {
+        $collection = collect([1,2,3,4,5,6,7,8,9,10]);
+
+        $result = $collection->chunk(3);
+
+        $this->assertEqualsCanonicalizing([1,2,3], $result->all()[0]->all());
+        $this->assertEqualsCanonicalizing([4,5,6], $result->all()[1]->all());
+        $this->assertEqualsCanonicalizing([7,8,9], $result->all()[2]->all());
+        $this->assertEqualsCanonicalizing([10], $result->all()[3]->all());
+    }
+
 
 }
