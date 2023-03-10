79c4316fb8b3128a4e205e202c9a767c02e220a0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-11 00:26:57 +0700

    Reduce

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -440,5 +440,22 @@ class CollectionTest extends TestCase
 
     }
 
+    public function testReduce()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        $result = $collection->reduce(function ($carry, $item){
+            return $carry + $item;
+        });
+        $this->assertEquals(45, $result);
+
+        // reduce(1,2) = 3
+        // reduce(3,3) = 6
+        // reduce(6,4) = 10
+        // reduce(10,5) = 15
+        // reduce(15,6) = 21
+        // reduce(21,7) = 28
+
+    }
+
 
 }
